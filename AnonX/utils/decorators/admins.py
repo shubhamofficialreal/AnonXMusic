3376d77e8124555dd00e8f4966e46540e83c5d84
shubhamofficialreal <122885957+shubhamofficialreal@users.noreply.github.com>
2023-02-01 10:08:11 +0530

    Update admins.py

--- AnonX/utils/decorators/admins.py
+++ AnonX/utils/decorators/admins.py
@@ -36,7 +36,7 @@ def AdminRightsCheck(mystic):
                     [
                         InlineKeyboardButton(
                             text="ʜᴏᴡ ᴛᴏ ғɪx ᴛʜɪs ?",
-                            callback_data="AnonymousAdmin",
+                            callback_data="BlazeShubham",
                         ),
                     ]
                 ]
@@ -93,7 +93,7 @@ def AdminActual(mystic):
                     [
                         InlineKeyboardButton(
                             text="ʜᴏᴡ ᴛᴏ ғɪx ᴛʜɪs ?",
-                            callback_data="AnonymousAdmin",
+                            callback_data="BlazeShubham",
                         ),
                     ]
                 ]
